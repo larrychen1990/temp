cec21f2e4c21f3b3cdac025e4f800615bb90d58a
larrychen1990 <larrychen1990@163.com>
2015-01-07 15:29:33 +0800

    mysql

---
+++ MySQL Files/connect.bat
@@ -0,0 +1,7 @@
+rem set homeDir
+set homeDir=~\Documents\Soft\mysql-5.6.22-win32
+cd %homeDir%\bin
+echo connect to mysql server...
+mysql -u test -p
+
+@pause
---
+++ MySQL Files/startServer.bat
@@ -0,0 +1,8 @@
+rem set homeDir
+set homeDir=~\Documents\Soft\mysql-5.6.22-win32
+cd %homeDir%\bin
+echo start mysql server...
+mysqld --console
+
+@pause
+
